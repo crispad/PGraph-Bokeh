9207f4471aef415230a42b8f2aaf3d7fca6eeb61
Cristina Laryea <crispad06@gmail.com>
2018-06-20 18:54:50 -0400

    fixed layout code, wip

--- src/draw.py
+++ src/draw.py
@@ -11,7 +11,7 @@ graph_data = Graph()
 graph_data.debug_create_test_data()
 print(graph_data.vertexes)
 
-N = 10
+N = len(graph_data.vertexes)
 node_indices = list(range(N))
 
 color_list = []
@@ -27,7 +27,7 @@ plot = figure(title='Graph Layout Demonstration', x_range=(0, 500), y_range=(0, 
 graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
-graph.node_renderer.data_source.add(Spectral8, 'color')
+graph.node_renderer.data_source.add(color_list, 'color')
 graph.node_renderer.glyph = Oval(height=10, width=10, fill_color='color')
 
 graph.edge_renderer.data_source.data = dict(
@@ -35,9 +35,9 @@ graph.edge_renderer.data_source.data = dict(
     end=node_indices)
 
 ### start of layout code
-circ = [i*2*math.pi/N for i in node_indices]
-x = [math.cos(i) for i in circ]
-y = [math.sin(i) for i in circ]
+
+x = [v.pos['x'] for v in graph_data.vertexes]
+y = [v.pos['y'] for v in graph_data.vertexes]
 
 graph_layout = dict(zip(node_indices, zip(x, y)))
 graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
