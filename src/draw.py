c4023d152b1dae3bdc77841fb97a2271551f6449
Cristina Laryea <crispad06@gmail.com>
2018-06-21 16:52:07 -0400

    added height and width,working on bfs code

--- src/draw.py
+++ src/draw.py
@@ -2,14 +2,19 @@ import math
 
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval, LabelSet, ColumnDataSource
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Circle, LabelSet, ColumnDataSource
 from bokeh.palettes import Spectral8
 
 from graph import *
 
+WIDTH = 640
+HEIGHT = 480
+#CIRCLE_SIZE = 30
+
 graph_data = Graph()
 graph_data.debug_create_test_data()
-print('+++', graph_data.vertexes)
+#graph_data.bfs(graph_data.vertexes[0])
+# print('+++', graph_data.vertexes)
 
 N = len(graph_data.vertexes)
 node_indices = list(range(N))
@@ -18,23 +23,34 @@ color_list = []
 for vertex in graph_data.vertexes:
     color_list.append(vertex.color)
 
-
+'''
 debug_pallete = Spectral8
 debug_pallete.append('ff0000')
 debug_pallete.append('0000ff')
-
-plot = figure(title='Graph Layout Demonstration', x_range=(0, 500), y_range=(0, 500),
+'''
+plot = figure(title='Graph Layout Demonstration', x_range=(0, HEIGHT), y_range=(0, WIDTH),
               tools='', toolbar_location=None)
 
 graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
 graph.node_renderer.data_source.add(color_list, 'color')
-graph.node_renderer.glyph = Oval(height=10, width=10, fill_color='color')
+graph.node_renderer.glyph = Circle(size=20, fill_color='color')
+
+# this is drawing the edges from start to end # in-class solution 
+'''
+start_indexes = []
+end_indexes = []
+
+for start_indexes, vertex in enumerate(graph_data.vertexes):
+    for e in vertex.edges:
+        start_indexes.append(start_index)
+        end_indexes.append(graph_data.vertexes.index(e.destination))
+'''
 
 graph.edge_renderer.data_source.data = dict(
-    start=[],
-    end=[])
+    start=[1, 2], # a list of vertex indexes to start edges from 
+    end=[3, 4]) # a list of vertex indexes to end edges at 
     
 for vertex in graph_data.vertexes:
         if len(vertex.edges) > 0:
@@ -52,7 +68,7 @@ y = [v.pos['y'] for v in graph_data.vertexes]
 
 source = ColumnDataSource(data=dict(x_pos=x, y_pos=y, names=['t1', 't2', 't3', 't4', 't5', 't6']))
 
-labels = LabelSet(x='x_pos', y='y_pos', text='names', level='glyph', x_offset=-10, y_offset=-10, source=source, render_mode='canvas')
+labels = LabelSet(x='x_pos', y='y_pos', text='names', level='glyph', x_offset=-10, y_offset=-10, source=source, render_mode='canvas', text_align='center', text_baseline='middle')
 
 graph_layout = dict(zip(node_indices, zip(x, y)))
 graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
--- src/graph.py
+++ src/graph.py
@@ -1,3 +1,5 @@
+import random
+
 class Edge: 
     def __init__(self, destination):
         self.destination = destination
@@ -16,12 +18,8 @@ class Graph:
     def debug_create_test_data(self):
         debug_vertex_1 = Vertex('t1', x=40, y=40)
         debug_vertex_2 = Vertex('t2', x=140, y=140)
-        debug_vertex_3 = Vertex('t3', x=160, y=200)
-        debug_vertex_4 = Vertex('t4', x=340, y=340)
-        debug_vertex_5 = Vertex('t5', x=200, y=250)
-        debug_vertex_6 = Vertex('t6', x=360, y=250)
-
-
+        debug_vertex_3 = Vertex('t3', x=300, y=400)
+        debug_vertex_4 = Vertex('t4', x=400, y=100)
 
 
 
@@ -34,15 +32,29 @@ class Graph:
         debug_edge_3 = Edge(debug_vertex_4)
         debug_vertex_3.edges.append(debug_edge_3)
 
-        debug_edge_4 = Edge(debug_vertex_5)
-        debug_vertex_4.edges.append(debug_edge_4)
-
-        debug_edge_5 = Edge(debug_vertex_6)
-        debug_vertex_5.edges.append(debug_edge_5)
-
-
 
         self.vertexes.extend([debug_vertex_1, debug_vertex_2])
         self.vertexes.extend([debug_vertex_3, debug_vertex_4])
-        self.vertexes.extend([debug_vertex_5, debug_vertex_6])
         
+
+def bfs(self, start):
+    random_color ="#" + ''.join[random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]       
+
+    queue = []
+    found = []
+
+    queue.append(start)
+    found.append(start)
+
+    start.color = random_color
+
+    while len(queue) > 0:
+        v = queue[0]
+        for edge in v.edges:
+            if edge.destination not in found:
+                found.append(edge.destination)
+                queue.append(edge.destination)
+                edge.destination.color = random_color
+        queue.pop(0) #TODO: look at collections.dequeue
+
+    return found
