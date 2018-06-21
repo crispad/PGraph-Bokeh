d4c1c2a5de38ecef762e374c16f79c891ec4fe91
Cristina Laryea <crispad06@gmail.com>
2018-06-20 22:36:32 -0400

    fixed vertexes, and edges, added labels

--- src/draw.py
+++ src/draw.py
@@ -2,7 +2,7 @@ import math
 
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval, LabelSet, ColumnDataSource
 from bokeh.palettes import Spectral8
 
 from graph import *
@@ -33,18 +33,33 @@ graph.node_renderer.data_source.add(color_list, 'color')
 graph.node_renderer.glyph = Oval(height=10, width=10, fill_color='color')
 
 graph.edge_renderer.data_source.data = dict(
-    start=[0]*N,
-    end=node_indices)
+    start=[],
+    end=[])
+    
+for vertex in graph_data.vertexes:
+        if len(vertex.edges) > 0:
+            for edge in vertex.edges:
+                start = graph_data.vertexes.index(vertex)
+                graph.edge_renderer.data_source.data['start'].append(start)
+
+                end = graph_data.vertexes.index(edge.destination)
+                graph.edge_renderer.data_source.data['end'].append(end)
 
 ### start of layout code
 
 x = [v.pos['x'] for v in graph_data.vertexes]
 y = [v.pos['y'] for v in graph_data.vertexes]
 
+source = ColumnDataSource(data=dict(x_pos=x, y_pos=y, names=['t1', 't2', 't3', 't4', 't5', 't6']))
+
+labels = LabelSet(x='x_pos', y='y_pos', text='names', level='glyph', x_offset=-10, y_offset=-10, source=source, render_mode='canvas')
+
 graph_layout = dict(zip(node_indices, zip(x, y)))
 graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
 plot.renderers.append(graph)
+
+plot.add_layout(labels)
 
 output_file('graph.html')
 show(plot)
